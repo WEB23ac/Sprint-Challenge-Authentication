8802fea92ab5f8ceac45ff4aae9e59d561857e7b
Aaron Clark <aaronpclark92@gmail.com>
2019-11-15 10:33:32 -0600

    added add, findById, findBy, find functions to users-model
    added testing to knexfile
    
    added jwt-helpers.js set token expiration at 10h
    
    added authenticate-middleware function

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,31 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
-const db = require('../helpers/users-model');
+const users = require('../helpers/users-model');
+
+const { getJwtToken } = require('../helpers/jwt-helpers');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const validationResult = validateUser(user);
+
+  if (validationResult.isSuccessful) {
+    const hash = bcrypt.hashSync(user.password, 14);
+    user.password = hash;
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved)
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Error creating user in Database', error: err.toStrin() })
+      })
+  } else {
+    res.status(400).json({
+      message: 'Invalid user information, see error for more information.',
+      error: validationResult.errors
+    })
+  }
 });
 
 router.post('/login', (req, res) => {
